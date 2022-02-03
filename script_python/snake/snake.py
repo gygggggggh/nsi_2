84b6f5390f0619a6e91ecc02db5bba7660a08ed3
gygggggggh <dylan.schertzer@oulook.fr>
2022-02-03 13:42:02 +0100

    bug git cant download last commit

--- script_python/snake/snake.py
+++ script_python/snake/snake.py
@@ -1,18 +1,32 @@
 import tkinter as tk
+import random
 
 f = tk.Tk()
-
 f.title('snake')
-
 f.iconphoto(True, tk.PhotoImage(file='script_python/snake/snake.png'))
-wl= f.winfo_reqwidth()
-wh = f.winfo_reqheight()
-print("Width",windowWidth,"Height",windowHeight)
+
+hexa= ["#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)])]
+
+f.config(background=hexa)
 
 
-f.geometry("500x500+450+100")
+
+# taille de la fenêtre
+hauteur_f = 500
+largeur_f = 500
+
+#taille de l'ecran 
+largeur_ecran = f.winfo_screenwidth()
+hauteur_ecran = f.winfo_screenheight()
+
+
+cor_x = int((largeur_ecran/2) - (largeur_f/2))
+cor_y = int((hauteur_ecran/2) - (hauteur_f/2))
+
+f.geometry("{}x{}+{}+{}".format(largeur_f, hauteur_f, cor_x, cor_y))
+
 f.resizable(0,0)
 
 
 if  __name__ == '__main__' :
-    f.mainloop()
+    f.mainloop()b
