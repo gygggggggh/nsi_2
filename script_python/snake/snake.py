72e971d2d5d198743d4c5c36b8456dc6be7bb5c8
gygggggggh <dylan.schertzer@oulook.fr>
2022-02-04 15:14:41 +0100

    create todo

--- script_python/snake/snake.py
+++ script_python/snake/snake.py
@@ -6,13 +6,6 @@ f.title('snake')
 f.iconphoto(True, PhotoImage(file='script_python/snake/snake.png'))
 
 
-# on choisis une couleur aleatoir en hexadecimal 
-hexa= ["#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)])]
-
-
-
-
-
 # taille de la fenêtre
 hauteur_f = 500
 largeur_f = 500
@@ -27,10 +20,13 @@ cor_y = int((hauteur_ecran/2) - (hauteur_f/2))
 f.geometry("{}x{}+{}+{}".format(largeur_f, hauteur_f, cor_x, cor_y))
 f.resizable(1,1)
 
-grille = Canvas(width=30, height=30, bg = hexa)
-grille.create_rectangle(100,100,100,100, outline='red',)
-grille.grid(row=0, column=0,)
+
+
+
+
+
 
 
 if  __name__ == '__main__' :
     f.mainloop()
+
