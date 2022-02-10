864d33ada2b11c2afc65219ac00ca7da284d78d9
gygggggggh <dylan.schertzer@outlook.fr>
2022-02-10 18:19:47 +0100

    avancemnt projet snake

--- script_python/snake/snake.py
+++ script_python/snake/snake.py
@@ -1,16 +1,13 @@
 
-import tkinter as tk
+from tkinter import *
 import random
 
-f = tk.Tk()
+f = Tk()
 f.title('snake')
-f.iconphoto(True, tk.PhotoImage(file='script_python/snake/snake.png'))
+f.iconphoto(True, PhotoImage(file='script_python/snake/snake.png'))
 
-hexa= ["#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)])]
-
-f.config(background=hexa)
-
-
+# variable pour stocker le score 
+score = 0
 
 # taille de la fenêtre
 hauteur_f = 500
@@ -27,6 +24,31 @@ cor_y = int((hauteur_ecran/2) - (hauteur_f/2))
 f.geometry("{}x{}+{}+{}".format(largeur_f, hauteur_f, cor_x, cor_y))
 
 f.resizable(0,0)
+
+grille = Canvas(f, width =500, height = 450, bg = "black", )
+grille.pack(side="bottom")
+
+score = Canvas(f, width =500, height = 60, bg = "white", relief = 'raised',  bd = '6')
+score.create_text(250,20, text = 'score: '+ str(0), font = ('Times', '24', 'bold '))
+score.pack(side="top")
+
+
+nombre_grille = 30
+
+Largeur_Case = (500 / nombre_grille)
+Hauteur_Case = (450 / nombre_grille)
+
+
+def remplir_case (x, y):
+    Origine_snake_X1 = x * Largeur_Case
+    Origine_snake_Y1 = y * Hauteur_Case
+    Origine_snake_X2 = Origine_snake_X1 + Largeur_Case
+    Origine_snake_Y2 = Origine_snake_Y1 + Hauteur_Case
+
+    grille.create_rectangle(Origine_snake_X1, Origine_snake_Y1, Origine_snake_X2, Origine_snake_Y2, fill="green")
+
+
+
 
 
 if  __name__ == '__main__' :
